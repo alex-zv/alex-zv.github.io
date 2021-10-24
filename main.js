025bf9238385d0384e4bec444ff0b45c1ff3c1b5
Oleksandr Zviahintsev <alex.zv.work@gmail.com>
2021-10-24 22:18:02 +0300

    update api

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const apiServer = 'http://localhost:3030/api';
+const apiServer = 'https://alex-pwa.herokuapp.com/api';
 function urlB64ToUint8Array(base64String) {
     const padding = '='.repeat((4 - base64String.length % 4) % 4);
     const base64 = (base64String + padding)
